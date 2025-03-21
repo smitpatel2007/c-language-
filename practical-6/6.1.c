6c87846ba941dd735c6303b4c5574f24f3a3e6c5
smit patel <smitpatel1347@gmail.com>
2025-03-21 20:26:35 +0530

    Update 6.1.c

--- practical-6/6.1.c
+++ practical-6/6.1.c
@@ -2,36 +2,36 @@
 
 int main() {
     // Integer array part
-    int arr[5];
+    int arr[10];
     int b = 0;
     float int_avg;
     int i;
 
     printf("Enter 10 integer numbers:\n");
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < 10; i++) {
         printf("Integer Number %d: ", i + 1);
         scanf("%d", &arr[i]);
         b= b + arr[i];
     }
 
-    int_avg = b / 5.0;
+    int_avg = b / 10.0;
 
     printf("\nInteger Array Result:\n");
     printf("Sum = %d\n", b);
     printf("Average = %.2f\n\n", int_avg);
 
     // Float array part
-    float a[5];
+    float a[10];
     float c = 0, float_avg;
 
     printf("Enter 10 float numbers:\n");
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < 10; i++) {
         printf("Float Number %d: ", i + 1);
         scanf("%f", &a[i]);
         c= c + a[i];
     }
 
-    float_avg = c/ 5;
+    float_avg = c/ 10;
 
     printf("\nFloat Array Result:\n");
     printf("Sum = %.2f\n", c);
