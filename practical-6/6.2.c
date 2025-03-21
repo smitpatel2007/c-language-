ebd2f743b561aeae96817384c2a8f1844ff81366
smit patel <smitpatel1347@gmail.com>
2025-03-21 20:27:53 +0530

    Update 6.2.c

--- practical-6/6.2.c
+++ practical-6/6.2.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num[5];
+    int i, a = 0, b = 1;
+  
+    printf("Enter 10 integer numbers:\n");
+    for (i = 0; i < 5; i++) {
+        printf("Number %d: ", i);
+        scanf("%d", &num[i]);
+    }
+    for (i = 0; i < 5; i++) {
+        if (i % 2 == 0) {
+            b = b * num[i];   
+        } else {
+            a = a + num[i];   
+        }
+    }
+    printf("\nSum of elements at odd indexes = %d\n", a);
+    printf("Multiplication of elements at even indexes = %d\n", b);
+
+    return 0;
+}
 
