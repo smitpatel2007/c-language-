dc8413b3edc82d256c30192acc255a28f2986c01
smit patel <smitpatel1347@gmail.com>
2025-03-21 20:30:11 +0530

    Update 6.3.c

--- practical-6/6.3.c
+++ practical-6/6.3.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+int main() {
+    int a[5], i, max;
+    printf("Enter 5 elements:\n");
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &a[i]);
+    }
+    max = a[0];
+    for (i = 1; i < 5; i++) {
+        if (a[i] > max) {
+            max = a[i];
+        }
+    }
+    printf("Maximum element = %d\n", max);
+    return 0;
+}
 
