c4d15ab27d7fb2167384b1c140ddaee7315f9bb2
smit patel <smitpatel1347@gmail.com>
2025-03-21 20:35:46 +0530

    Update 6.5.c

--- practical-6/6.5.c
+++ practical-6/6.5.c
@@ -1,1 +1,19 @@
 
+#include <stdio.h>
+int main() {
+    int a[2][2], i, j;
+    printf("Enter 2x2 matrix elements:\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("Transpose of the matrix:\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            printf("%d ", a[j][i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
