069a5c60fee071fa871b2a13ca8602261a1f45cf
smit patel <smitpatel1347@gmail.com>
2025-03-24 21:09:15 +0530

    Update 6.7.c

--- practical-6/6.7.c
+++ practical-6/6.7.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+int main() {
+    int array[10], size, i;
+    int largestOdd = -1; 
+    printf("Enter the size of the array (max 10): ");
+    scanf("%d", &size);
+    printf("Enter %d elements:\n", size);
+    for(i = 0; i < size; i++) {
+        scanf("%d", &array[i]);
+    }
+    for(i = 0; i < size; i++) {
+        if(array[i] % 2 != 0) { 
+            if(largestOdd == -1 || array[i] > largestOdd) {
+                largestOdd = array[i];
+            }
+        }
+    }
+    if(largestOdd == -1) {
+        printf("No odd numbers found in the array.\n");
+    } else {
+        printf("The largest odd number is: %d\n", largestOdd);
+    }
 
+    return 0;
+}
