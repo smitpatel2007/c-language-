8dbbb5a898525c0a4f153b18a3d4ba40a76faa94
smit patel <smitpatel1347@gmail.com>
2025-03-25 21:21:49 +0530

    Update 6.6.c

--- practical-6/6.6.c
+++ practical-6/6.6.c
@@ -1,33 +1,48 @@
 #include <stdio.h>
 int main() {
-    int matrix1[3][3], matrix2[3][3], result[3][3];
+    int a[3][3], b[3][3],c[3][3];
     int i, j, k;
     printf("Enter elements of first 3x3 matrix:\n");
     for(i = 0; i < 3; i++) {
         for(j = 0; j < 3; j++) {
             printf("Enter element [%d][%d]: ", i, j);
-            scanf("%d", &matrix1[i][j]);
+            scanf("%d", &a[i][j]);
         }
+    }
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("%d ", a[i][j]);
+            
+        }
+        printf("\n");
     }
     printf("\nEnter elements of second 3x3 matrix:\n");
     for(i = 0; i < 3; i++) {
         for(j = 0; j < 3; j++) {
             printf("Enter element [%d][%d]: ", i, j);
-            scanf("%d", &matrix2[i][j]);
+            scanf("%d", &b[i][j]);
         }
+    }
+    
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("%d ", b[i][j]);
+            
+        }
+        printf("\n");
     }
     for(i = 0; i < 3; i++) {
         for(j = 0; j < 3; j++) {
-            result[i][j] = 0;
+           c[i][j] = 0;
             for(k = 0; k < 3; k++) {
-                result[i][j] += matrix1[i][k] * matrix2[k][j];
+               c[i][j] += a[i][k] * b[k][j];
             }
         }
     }
     printf("\nResult of matrix multiplication:\n");
     for(i = 0; i < 3; i++) {
         for(j = 0; j < 3; j++) {
-            printf("%d\t", result[i][j]);
+            printf("%d\t", c[i][j]);
         }
         printf("\n");
     }
