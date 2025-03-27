5793414a1ee7a04ac92f9ba6ded9bc9be57aa463
smit patel <smitpatel1347@gmail.com>
2025-03-27 22:58:15 +0530

    Update 7.3.c

--- practical-7/7.3.c
+++ practical-7/7.3.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+int findMax(int a, int b, int c);
+
+int main() {
+    int num1, num2, num3, max;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    max = findMax(num1, num2, num3);
+    printf("The maximum number is: %d\n", max);
+
+    return 0;
+}
+int findMax(int a, int b, int c) {
+    int max;
+    
+    if (a >= b && a >= c) 
+        max = a;
+    else if (b >= a && b >= c) 
+        max = b;
+    else 
+        max = c;
+
+    return max; 
+}
 
