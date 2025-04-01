463284646dccfe1b9688306942e7354878ecf159
smit patel <smitpatel1347@gmail.com>
2025-04-01 20:02:00 +0530

    Update 7.4.c

--- practical-7/7.4.c
+++ practical-7/7.4.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+
+int factorial(int n);
+
+int main() {
+    int num, fact;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    fact = factorial(num);
+    printf("Factorial of %d = %d\n", num, fact);
+
+    return 0;
+}
+
+int factorial(int n) {
+    int i, fact = 1;
+
+    for (i = 1; i <= n; i++) {
+        fact *= i;
+    }
+
+    return fact;
+}
+
+
+
 
