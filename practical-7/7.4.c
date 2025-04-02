b30ece5310f14b5a6b64c8be8837fe465fda4255
smit patel <smitpatel1347@gmail.com>
2025-04-02 17:11:05 +0530

    Update 7.4.c

--- practical-7/7.4.c
+++ practical-7/7.4.c
@@ -1,4 +1,32 @@
+
 //(a) With the help recursion
+
+#include<stdio.h>
+
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int number;
+    printf("Enter a number to calculate its factorial: ");
+    scanf("%d", &number);
+    
+    if (number < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        printf("Factorial of %d is %d\n", number, factorial(number));
+    }
+
+    return 0;
+}
+
+
+//(b) Without the help recursion
 
 #include <stdio.h>
 
@@ -28,32 +56,6 @@ int factorial(int n) {
 
 
 
-
-//(b) Without the help recursion
-
-#include<stdio.h>
-
-int factorial(int n) {
-    if (n == 0) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
-}
-
-int main() {
-    int number;
-    printf("Enter a number to calculate its factorial: ");
-    scanf("%d", &number);
-    
-    if (number < 0) {
-        printf("Factorial is not defined for negative numbers.\n");
-    } else {
-        printf("Factorial of %d is %d\n", number, factorial(number));
-    }
-
-    return 0;
-}
 
 
 
