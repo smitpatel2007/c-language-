63aac8a37d88c492ba4856477fd849ab12d59e93
smit patel <smitpatel1347@gmail.com>
2025-04-04 16:53:51 +0530

    Create functionmatrix.c

---
+++ practical-7/functionmatrix.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+void transposematrix();
+int main() {
+    transposematrix(); 
+    return 0;
+}
+
+void transposematrix() {
+    int a[2][2],b[2][2],c[2][2] ,i, j,k;
+    printf("Enter elements of first 2x2 matrix:\n");
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 2; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("\nEnter elements of second 2x2 matrix:\n");
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 2; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+    printf("Transpose of the matrix:\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            printf("%d ", a[j][i]);
+        }
+        printf("\n");
+    }
+    // multiplication
+    
+    
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 2; j++) {
+           c[i][j] = 0;
+            for(k = 0; k < 2; k++) {
+               c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+    
+    printf("\nResult of matrix multiplication:\n");
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 2; j++) {
+            printf("%d\t", c[i][j]);
+        }
+        printf("\n");
+    }
+    printf("result of matrix addition\n");
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
+            printf("%d\t", c[i][j]);
+        }
+        printf("\n");
+    }
+
+}
