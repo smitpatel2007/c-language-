6541768d1877c58ddc0360a00a40695eac533770
smit patel <smitpatel1347@gmail.com>
2025-04-10 21:35:37 +0530

    Update extern.c

--- practical_8/8_3/extern.c
+++ practical_8/8_3/extern.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+
+// Declaring an extern variable (defined in extern_var.c)
+extern char message[];
+
+int main() {
+    printf("%s\n", message);  // Accessing the extern variable
+    return 0;
+}
 
