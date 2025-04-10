11502dbcde2b5f1a011053a342ec1cbb8ebba5b9
smit patel <smitpatel1347@gmail.com>
2025-04-10 21:34:21 +0530

    Update and rename practical_8/8_3.c to practical_8/8_3/main.c

---
+++ practical_8/8_3/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// Declaring an extern variable (defined in extern_var.c)
+extern char message[];
+
+int main() {
+    printf("%s\n", message);  // Accessing the extern variable
+    return 0;
+}
