eb3ddb29ff0cb9338cb42af285582ae505933d4b
smit patel <smitpatel1347@gmail.com>
2025-04-11 16:49:26 +0530

    Update 8_3.c

--- practical_8/8_3.c
+++ practical_8/8_3.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+// Global variable declared
+int number = 10;  // Variable 
+void displayNumber() {
+    extern int number; // Using extern for access global variable
+    printf("The value of number is: %d\n", number);
+}
+
+int main() {
+    displayNumber(); // function calling for output
+    return 0;
+}
