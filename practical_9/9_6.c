acb8f8e1411670ca46bc99a22b44df373ea050db
smit patel <smitpatel1347@gmail.com>
2025-05-03 21:28:42 +0530

    Update 9_6.c

--- practical_9/9_6.c
+++ practical_9/9_6.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+void swap(int *a, int *b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+int main() {
+    int a=8, b=5;
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+    swap(&a, &b);
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    
+    return 0;
+}
 
