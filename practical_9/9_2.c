fac8af3d7f1142963365051c852027837678de50
smit patel <smitpatel1347@gmail.com>
2025-05-12 23:21:29 +0530

    Update 9_2.c

--- practical_9/9_2.c
+++ practical_9/9_2.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Game {
+    char player_name[50];
+    char team_name[50];
+    int max_score;
+};
+
+int main() {
+    struct Game Player[3];
+    int i;
+
+    for (i = 0; i < 3; i++) {
+        printf("\nEnter details for Player %d:\n", i + 1);
+
+        printf("Enter Player Name: ");
+        scanf("%s", Player[i].player_name);
+
+        printf("Enter Team Name: ");
+        scanf("%s", Player[i].team_name);
+
+        printf("Enter Maximum Score: ");
+        scanf("%d", &Player[i].max_score);
+    }
+
+    printf("\n--- Team-wise Player Details ---\n");
+    for (i = 0; i < 3; i++) {
+        printf("Team: %s   | Player: %s   | Maximum Score: %d\n", 
+               Player[i].team_name, Player[i].player_name, Player[i].max_score);
+    }
+
+    return 0;
+}
 
